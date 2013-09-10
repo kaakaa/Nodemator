1e18754e5726d66315929081ef2b4c5e529e2b47
kaakaa <stooner.hot@gamil.com>
2013-09-10 21:48:54 +0900

    apply haml.js

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var engines = require('consolidate');
+var fs = require('fs');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'haml');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -22,12 +24,20 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('haml', engines.haml);
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
+app.get('/', function(req, res) {
+	res.render('index', {
+			sample: 'sample',
+			hoge: 'hoge'
+	});
+});
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
